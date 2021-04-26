e869b802b9c7aba262ec6b89a5af672f2e180694
Max <max.shugar@sony.com>
2021-04-26 17:05:21 +0100

    Implemented error handling of scripts.

--- src/supervisor/index.js
+++ src/supervisor/index.js
@@ -1,16 +1,26 @@
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
+
 const validateRequestAgainstSchema = require("./validate_request");
 const executeSchema = require("./schemas/execute_schema.json");
 const engine = require('../engine');
 
-const app = express();
-app.use(bodyParser.json());
 const port = 4000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
 
 app.post("/execute", async (req, res) => {
-  if (validateRequestAgainstSchema(req.body, executeSchema) < 0)
+
+  console.log(req.body);
+
+  if (validateRequestAgainstSchema(req.body, executeSchema) < 0){
+    console.log("schema error")
     return res.status(400).json({ success: false, body: "schema error" });
+  }
+    
   
   const {code, language, blocking} = req.body;
   let ret = await engine.execute(code);
